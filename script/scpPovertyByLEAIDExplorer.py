495825d30859b59c10b87c135cda0a0d967ceda2
jehibdon <124072172+jehibdon@users.noreply.github.com>
2023-07-20 17:19:11 -0400

    Made edit to poverty script

--- script/scpPovertyByLEAIDExplorer.py
+++ script/scpPovertyByLEAIDExplorer.py
@@ -10,5 +10,5 @@ sFile = './data/poverty_ussd17.xls'
 
 # db = pd.read_excel(sFile)
 dFrame = pd.read_excel(sFile, sheet_name='USSD17')
-dFrame.drop(dFrame.iloc[1:2,:],inplace=True,axis=0)
-print(dFrame)
+dFrame2 = dFrame.tail(-2)
+print(dFrame2)
