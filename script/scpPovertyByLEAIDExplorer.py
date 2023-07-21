62b993f85d83da1c5d6bb132c56e85a834d5e541
Matt Salomone <msalomone@gmail.com>
2023-07-21 08:45:15 -0400

    Generate poverty-by-district histogram

--- script/scpPovertyByLEAIDExplorer.py
+++ script/scpPovertyByLEAIDExplorer.py
@@ -8,7 +8,22 @@ from repoExplorer import funCSVCol, funFile2Lst, funGetSublist
 
 sFile = './data/poverty_ussd17.xls'
 
-# db = pd.read_excel(sFile)
-dFrame = pd.read_excel(sFile, sheet_name='USSD17')
-dFrame2 = dFrame.tail(-2)
-print(dFrame2)
+dFrame = pd.read_excel(sFile, sheet_name=0).tail(-2)
+vNames = ['STATE_POSTAL', 'STATE_FIPS', 'DIST_ID', 'DIST_NAME', 'EST_POP', 'EST_POP0517', 'EST_POV_POP0517']
+dFrame.columns = vNames
+
+#LEA_ID is STATE_FIPS concatenated with DIST_ID
+lLeaids = np.core.defchararray.add(dFrame['STATE_FIPS'].tolist(),dFrame['DIST_ID'].tolist())
+
+#Histogram generate (not very illuminating)
+plt.figure(1)
+plt.hist(dFrame['EST_POV_POP0517'].tolist(), 50)
+plt.xlabel('Number of Children 5-17 in Poverty')
+plt.ylabel('Number of School Districts')
+plt.title('Number of Children in Poverty by School District')
+plt.savefig('./figure/childPovertyDistrictHist.png')
+
+
+
+print('Complete.\n')
+
